68a9ea33c7ecebc52b2cf9b3f21e1f7049d01de1
barnden <brandon@barnden.dev>
2021-11-07 18:48:20 -0600

    node.js: Add minimal express web server

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@
 
 global.__basedir = __dirname
 
-const { join } = require("path")
-const { Database, User } = require(join(__basedir, "database", "firestore.js"))
+const path = require("path")
+const express = require("express")
+const { Database, User } = require(path.join(__basedir, "database", "firestore.js"))
+
 const db = new Database()
+const app = express()
+const port = 8000
+
+// Serve CSS/JS as static files
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", (req, res) => {
+    res.send("hi.")
+})
+
+app.listen(port, _ => {
+    console.log(`Listening at http://localhost:${port}`)
+})
