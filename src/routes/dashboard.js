8a70feba9644033f375e0d17b77531f56899321f
feierc <feierc@tamu.edu>
2021-11-16 12:40:43 -0600

    Added add transaction functionality to dashboard

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -8,7 +8,8 @@ const { Database } = require("../backend/firestore/database")
 const router = express.Router()
 
 module.exports = view => {
-    router.get("/", async(req, res) => {
+    router.
+    get("/", async(req, res) => {
         if(req.session.loggedIn == false) {
             return res.redirect("/login")
         } else {
@@ -51,6 +52,30 @@ module.exports = view => {
             }))
         }
         
+    })
+    .post("/add", async(req, res) => {
+        const { type, category, amount, date, note } = req.body;
+
+        const anyEmpty = (...args) => Array.from(args).reduce((acc, cur) => acc |= cur === "", false)
+        const user = new User("kornelharm@gmail.com") 
+        // Check to see if any field was left blank
+        if (anyEmpty(type, category, amount, date)) {
+            req.session.error = "Please fill out all fields"
+            return req.session.save(_ => res.redirect("/dashboard"))
+        }
+        
+        //not working 
+
+        if(req.body.type.toLowerCase() == "income") {
+            await user.addIncome(req.body.category,new Date(req.body.date),parseInt(req.body.amount),req.body.note)
+        } else if (req.body.type.toLowerCase() == "expense") {
+            await user.addExpense(req.body.category,new Date(req.body.date),parseInt(req.body.amount),req.body.note)
+        } else {
+            req.session.error = "Please enter a valid type of transaction"
+            return req.session.save(_ => res.redirect("/dashboard"))
+        }
+
+        return res.redirect("/dashboard")
     })
 
     return router
