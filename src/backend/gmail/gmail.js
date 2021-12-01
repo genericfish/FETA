5fdd8551e92a10bf6de976f4175e485075eef8e2
barnden <brandon@barnden.dev>
2021-12-01 17:44:43 -0600

    Gmail: Correctly implement the Gmail API

--- src/backend/gmail/gmail.js
+++ src/backend/gmail/gmail.js
@@ -1,90 +1,101 @@
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
+const fs = require('fs')
+const readline = require('readline')
+const { google } = require('googleapis')
+class Gmail {
+    static gScopes = ["https://www.googleapis.com/auth/gmail.send"]
+    static gCredentialsPath = process.env.OAUTH_CLIENT_CREDENTIALS || "credentials.json"
+    static gTokenPath = process.env.OAUTH_CLIENT_TOKEN || "token.json"
+    static gSender = process.env.MAILING_LIST_SENDER || "taeleshotsauce@gmail.com"
+    static gOAuthClient = null
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/gmail.send'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json';
+    static {
+        if (!fs.existsSync(this.gCredentialsPath)) {
+            console.error(`[Gmail] Cannot find credentials file "${this.gCredentialsPath}"`)
+            process.exit(1)
+        }
 
-// Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Gmail API.
-  authorize(JSON.parse(content), listLabels);
-});
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-/**
- * Lists the labels in the user's account.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listLabels(auth) {
-  const gmail = google.gmail({version: 'v1', auth});
-  gmail.users.labels.list({
-    userId: 'me',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const labels = res.data.labels;
-    if (labels.length) {
-      console.log('Labels:');
-      labels.forEach((label) => {
-        console.log(`- ${label.name}`);
-      });
-    } else {
-      console.log('No labels found.');
+        this.#authorize()
     }
-  });
+
+    static #authorize() {
+        const credentials = fs.readFileSync(this.gCredentialsPath)
+        const { client_secret, client_id, redirect_uris } = JSON.parse(credentials).installed
+        this.gOAuthClient = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
+
+        if (!fs.existsSync(this.gTokenPath))
+            return this.#getNewToken()
+
+        const token = fs.readFileSync(this.gTokenPath)
+
+        this.gOAuthClient.setCredentials(JSON.parse(token))
+    }
+
+    static #getNewToken() {
+        const authURL = gOAuthClient.generateAuthUrl({
+            access_type: "offline",
+            scope: this.gScopes
+        })
+
+        console.log("Open this URL for authorization token: ", authURL)
+
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        })
+
+        rl.question("Enter token: ", code => {
+            rl.close()
+
+            this.gOAuthClient.getToken(code, (err, token) => {
+                if (err) {
+                    console.error("[GMail] Error retrieving access token")
+                    process.exit(1)
+                }
+
+                this.gOAuthClient.setCredentials(token)
+
+                fs.writeFile(this.gTokenPath, JSON.stringify(token), err => {
+                    if (err)
+                        return console.error(err)
+
+                    console.log("Stored token at", this.gTokenPath)
+                })
+            })
+        })
+    }
+
+    static #createEmail(recipient, subject, message) {
+        const request = [
+            'Content-Type: text/plain; charset="UTF-8"\n',
+            'MIME-Version: 1.0\n',
+            'Content-Transfer-Encoding: 7bit\n',
+            `to: ${recipient}\n`,
+            `from: ${this.gSender}\n`,
+            `subject: ${subject}\n\n`,
+            message
+        ].join('')
+
+        return Buffer.from(request, "utf8").toString("base64")
+    }
+
+    static send(recipient, subject, message) {
+        if (this.gOAuthClient === null)
+            return console.error("[Gmail] Cannot send email because gOAuthClient is not setup.")
+
+        const gmail = google.gmail({ version: "v1", auth: this.gOAuthClient })
+        gmail.users.messages.send({
+            userId: "me",
+            resource: {
+                raw: this.#createEmail(recipient, `FETA: ${subject}`, message)
+            }
+        }, (err, res) => {
+            if (err) {
+                console.error(`[Gmail] Failed to send email to "${recipient}" with subject "${subject}".`)
+                console.error(err)
+                return
+            }
+        })
+    }
 }
+
+module.exports = { Gmail: Gmail }
