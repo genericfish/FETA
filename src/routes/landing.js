ec94da1653b4400ecd10197c2840716d3a60b4e5
barnden <brandon@barnden.dev>
2021-12-05 02:51:30 -0600

    Landing: Redirect user to dashboard if logged in

--- src/routes/landing.js
+++ src/routes/landing.js
@@ -4,9 +4,14 @@ const express = require("express")
 const router = express.Router()
 
 module.exports = view => {
-    router.get("/", (req, res) => res.send(view({
-        header: "Financial Excellence Tracking Application"
-    })))
+    router.get("/", (req, res) => {
+        if (req.session.loggedIn === true)
+            return res.redirect("/dashboard")
+
+        res.send(view({
+            header: "Financial Excellence Tracking Application"
+        }))
+    })
 
     return router
 }
