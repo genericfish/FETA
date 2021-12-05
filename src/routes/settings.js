c43e9f8659bc100214475d428a0533fba5619014
barnden <brandon@barnden.dev>
2021-12-05 12:37:19 -0600

    Settings: Add blank page

---
+++ src/routes/settings.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const express = require("express")
+const path = require("path")
+const { User } = require(path.join(__basedir, "backend", "firestore"))
+const { Money, RFC3339, anyEmpty } = require(path.join(__basedir, "backend", "utils"))
+const router = express.Router()
+
+module.exports = view => {
+    router.get("/", async (req, res) => {
+        if (req.session.loggedIn !== true)
+            return res.redirect("/login")
+
+        const user = new User(req.session.email)
+
+        let name = await user.reference.get()
+
+        res.send(view({
+            header: "Settings",
+            name: name.data().firstname
+        }))
+    })
+
+    return router
+}
