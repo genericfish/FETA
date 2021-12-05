9ca5eada0fd3563221c1277bb442d4107ce341cf
feierc <feierc@tamu.edu>
2021-12-05 00:04:50 -0600

    added NMT functions

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -2,6 +2,7 @@
 
 const express = require("express")
 const path = require("path")
+const { getSystemErrorMap } = require("util")
 const { User } = require(path.join(__basedir, "backend", "firestore"))
 const { Money } = require(path.join(__basedir, "backend", "utils"))
 const router = express.Router()
@@ -39,10 +40,31 @@ module.exports = view => {
                     a.push([id, "expense", new Money(expense).Display, date, note, category, expense_array[j].data().date])
                 }
             }
+
+            let item_list = await user.getNMTItems() 
+            let b = []
+            for (let i = 0; i < item_list.length; i++) {
+                let NMT_array = await user.getNMTTransactions(item_list[i].id)
+                if(NMT_array != undefined) {
+                    for (let j = 0; j < NMT_array.length; j++) {
+                        let amount = NMT_array[j].data().amount
+                        let date = NMT_array[j].data().date.toDate().toDateString()
+                        let note = NMT_array[j].data().note
+                        let item = NMT_list[i].id
+                        let id = NMT_array[j].id
+                        b.push([id, amount, date, note, item, NMT_array[j].data().date])
+                    }
+                } 
+            }
+
+
             a.sort(function (a, b) { return b[6] - a[6] })
+            b.sort(function (a, b) { return b[5] - a[5] })
+
             res.send(view({
                 header: "Transactions",
-                transactions: a
+                transactions: a, 
+                NMTs: b
             }))
         })
         .post("/add", async (req, res) => {
@@ -113,5 +135,112 @@ module.exports = view => {
             }
             return res.redirect("/transaction")
         })
+        .post("/addItem", async (req, res) => {
+            if (req.session.loggedIn !== true)
+                return res.redirect("/login")
+
+            const { name, note } = req.body
+            const anyEmpty = (...args) => Array.from(args).reduce((acc, cur) => acc |= cur === "", false)
+            const user = new User(req.session.email)
+            // Check to see if any field was left blank
+            if (anyEmpty(name)) {
+                req.session.error = "Please fill out all fields"
+                return req.session.save(_ => res.redirect("/transaction"))
+            }
+
+            await user.addNMT(req.body.name, req.body.note)
+
+            return res.redirect("/transaction")
+        })
+        .post("/editItem", async (req, res) => {
+            if (req.session.loggedIn !== true)
+                return res.redirect("/login")
+
+            const { name, amount, note } = req.body
+            const anyEmpty = (...args) => Array.from(args).reduce((acc, cur) => acc |= cur === "", false)
+            const user = new User(req.session.email)
+            // Check to see if any field was left blank
+            if (anyEmpty(name, amount)) {
+                req.session.error = "Please fill out all fields"
+                return req.session.save(_ => res.redirect("/transaction"))
+            }
+
+            const NMT = { current: parseInt(req.body.amount), note: req.body.note }
+
+            await user.modifyNMT(req.body.name, NMT)
+
+            return res.redirect("/transaction")
+        })
+        .post("/removeItem", async (req, res) => {
+            if (req.session.loggedIn !== true)
+                return res.redirect("/login")
+
+            const { name} = req.body
+            const anyEmpty = (...args) => Array.from(args).reduce((acc, cur) => acc |= cur === "", false)
+            const user = new User(req.session.email)
+            // Check to see if any field was left blank
+            if (anyEmpty(name)) {
+                req.session.error = "Please fill out all fields"
+                return req.session.save(_ => res.redirect("/transaction"))
+            }
+            
+            await user.deleteNMT(req.body.name)
+
+            return res.redirect("/transaction")
+        })
+        .post("/removeNMT", async (req, res) => {
+            if (req.session.loggedIn !== true)
+                return res.redirect("/login")
+
+            const { item, ID } = req.body
+            const anyEmpty = (...args) => Array.from(args).reduce((acc, cur) => acc |= cur === "", false)
+            const user = new User(req.session.email)
+            // Check to see if any field was left blank
+            if (anyEmpty(item, ID)) {
+                req.session.error = "Please fill out all fields"
+                return req.session.save(_ => res.redirect("/transaction"))
+            }
+
+            await user.removeNMTTransaction(req.body.item, req.body.ID)
+            return res.redirect("/transaction")
+        })
+        .post("/editNMT", async (req, res) => {
+            if (req.session.loggedIn !== true)
+                return res.redirect("/login")
+
+            const { item, ID, date, note, amount } = req.body
+            const anyEmpty = (...args) => Array.from(args).reduce((acc, cur) => acc |= cur === "", false)
+            const user = new User(req.session.email)
+            // Check to see if any field was left blank
+            if (anyEmpty(item, ID, date, note, amount)) {
+                req.session.error = "Please fill out all fields"
+                return req.session.save(_ => res.redirect("/transaction"))
+            }
+
+            const NMTtransaction = { amount: parseInt(req.body.amount), date: new Date(req.body.date), note: req.body.note}  
+            await user.modifyNMTTransaction(req.body.item, req.body.ID, NMTtransaction)
+            
+            return res.redirect("/transaction")
+        })
+        .post("/addNMT", async (req, res) => {
+            if (req.session.loggedIn !== true)
+                return res.redirect("/login")
+
+            const { item, amount, date, note } = req.body
+
+            const anyEmpty = (...args) => Array.from(args).reduce((acc, cur) => acc |= cur === "", false)
+            const user = new User(req.session.email)
+
+            // Check to see if any field was left blank
+            if (anyEmpty(item, amount, date)) {
+                req.session.error = "Please fill out all fields"
+                return req.session.save(_ => res.redirect("/transaction"))
+            }
+
+            await user.addNMTTransaction(req.body.item, parseInt(req.body.amount), new Date(req.body.date), req.body.note)
+
+            return res.redirect("/transaction")
+        })
+
     return router
 }
